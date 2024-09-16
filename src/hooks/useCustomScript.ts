442e280b353f11701017c6697c0ed1103eb16dbc
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-09-16 10:23:45 -0400

    useCustomScript functionality (poc for other projects)

---
+++ src/hooks/useCustomScript.ts
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+export default function useCustomScript(url: string) {
+    useEffect(() => {
+        const head = document.querySelector("head");
+        const newScriptTag = document.createElement("script");
+        newScriptTag.setAttribute("src", url);
+        head?.appendChild(newScriptTag);
+
+        return () => {
+            head?.removeChild(newScriptTag);
+        };
+    }, [url]);
+}

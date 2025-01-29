80e7946edefb5a91584d25ad0962d21815600443
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-01-29 12:35:46 -0500

    prettier

--- src/components/CurrentFavoriteMusic.tsx
+++ src/components/CurrentFavoriteMusic.tsx
@@ -1,5 +1,12 @@
 export default function CurrentFavoriteMusic() {
     return (
-        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0" height="450" style={{width: "100%", overflow: "hidden", borderRadius: "10px"}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/current-favorites/pl.u-leyl2zYfMVW3VWM"></iframe>
+        <iframe
+            allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+            frameBorder="0"
+            height="450"
+            style={{ width: "100%", overflow: "hidden", borderRadius: "10px" }}
+            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+            src="https://embed.music.apple.com/us/playlist/current-favorites/pl.u-leyl2zYfMVW3VWM"
+        ></iframe>
     );
 }

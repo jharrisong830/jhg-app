ead51f235e92606b7cb7e2c601a33d1ce2add894
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-12-21 16:12:33 -0500

    added home page only

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { APITester } from "./APITester";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import Home from "./pages/Home";
 
-export function App() {
-  return (
-    <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-      </div>
-
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
-    </div>
-  );
+export default function App() {
+    return <Home />;
 }
-
-export default App;

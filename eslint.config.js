940cc57171cd5e54c52785d21affc92a712b8f04
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-02-21 19:18:33 -0500

    using vite eslint config for react project

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,23 @@
-// @ts-check
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-import eslint from "@eslint/js";
-import { defineConfig, globalIgnores } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  globalIgnores(["dist/**"]),
+export default defineConfig([
+  globalIgnores(['dist']),
   {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  }
-);
+  },
+])

5e2ae8ad6e9bb1e00c21d0d676f66f077615550d
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-02-21 19:04:17 -0500

    add eslint and mui

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  globalIgnores(["dist/**"]),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  }
+);
